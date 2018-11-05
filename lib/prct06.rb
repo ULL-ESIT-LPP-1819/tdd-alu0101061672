dcab7f270107eeb90ef1552f70d3935a642d679a
Sonia Díaz Santos <alu0101061672@ull.edu.es>
2018-11-05 16:56:43 +0000

    Inicialización realizada con éxito

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -1,4 +1,5 @@
 require "prct06/version"
+require "prct06/nutricion"
 
 module Prct06
   # Your code goes here...
--- lib/prct06/nutricion.rb
+++ lib/prct06/nutricion.rb
@@ -1,3 +1,12 @@
 class Etiqueta
 
+	attr_accessor  :nombreEtiqueta, :grasas, :grasasSaturadas, :grasasMonoinsaturadas, :grasasPoliinsaturadas, :hidratosCarbono, :azucares, :polialcoholes, :almidon, :fibraAlimentaria, :proteínas, :sal, :vitaminas, :minerales
+
+
+	def initialize(nombreEtiqueta, grasas, grasasSaturadas, grasasMonoinsaturadas, grasasPoliinsaturadas, hidratosCarbono, azucares, polialcoholes, almidon, fibraAlimentaria, proteínas, sal, vitaminas, minerales)
+	
+		@nombreEtiqueta, @grasas, @grasasSaturadas, @grasasMonoinsaturadas, @grasasPoliinsaturadas, @hidratosCarbono, @azucares, @polialcoholes, @almidon, @fibraAlimentaria, @proteínas, @sal, @vitaminas, @minerales = nombreEtiqueta, grasas, grasasSaturadas, grasasMonoinsaturadas, grasasPoliinsaturadas, hidratosCarbono, azucares, polialcoholes, almidon, fibraAlimentaria, proteínas, sal, vitaminas, minerales
+	end
+
+
 end
