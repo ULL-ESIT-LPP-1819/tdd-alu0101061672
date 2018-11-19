a7e9a6b9e309a9933a391c70edd655604924d1f5
Sonia Díaz Santos <alu0101061672@ull.edu.es>
2018-11-19 18:41:32 +0000

    Fallo porque falta un nuevo argumento: sexo y porque la clase Persona no tiene declarado el método to_s

---
+++ lib/prct06/paciente.rb
@@ -0,0 +1,110 @@
+class Persona
+
+        attr_accessor :nombre, :apellidos, :edad, :direccion, :telefono
+
+        def initialize(nombre,apellidos,edad,direccion,telefono)
+
+        @nombre,@apellidos = nombre,apellidos
+        @edad,@direccion,@telefono = edad,direccion,telefono
+
+        end
+
+
+
+end
+
+class DatosAntropometricos < Persona
+
+        attr_accessor :peso, :talla, :edad, :sexo, :circunferenciaCintura, :circunferenciaCadera
+
+        def initialize (peso,talla,edad,sexo, circunferenciaCintura, circunferenciaCadera)
+
+                @peso, @talla, @edad, @sexo, @circunferenciaCintura, @circunferenciaCadera = peso,talla, edad, sexo, circunferenciaCintura, circunferenciaCadera
+
+        end
+
+        def to_s
+                "(#{@peso},#{@talla},#{@edad},#{@sexo},#{@circunferenciaCintura},#{@circunferenciaCadera})"
+        end
+
+        def IMC
+
+                imc = (peso/(talla*talla)).round(2)
+
+                if imc < 18.5
+                        "(#{imc})" #"IMC = #{imc}; Clasificación de la OMS: Bajo peso\nDescripción popular: Delgado"
+
+                elsif 18.5<=imc && imc<=24.9
+                        "(#{imc})"#"IMC = #{imc}; Clasificación de la OMS: Adecuado\nDescripción popular: Aceptable"
+
+                elsif 25.0<=imc && imc<=29.9
+                        "(#{imc})"#"IMC = #{imc}; Clasificación de la OMS: Sobrepeso\nDescripción popular: Sobrepeso"
+
+                elsif 30.0<=imc && imc<=34.9
+                        "(#{imc})"#"IMC = #{imc}; Clasificación de la OMS: Obesidad grado 1\nDescripción popular: Obesidad"
+
+                elsif 35.0<=imc && imc<=39.9
+                        "(#{imc})"#"IMC = #{imc}; Clasificación de la OMS: Obesidad grado 2\nDescripción popular: Obesidad"
+
+                elsif imc>=40
+                        "(#{imc})"#"IMC = #{imc}; Clasificación de la OMS: Obesidad grado 3\nDescripción popular: Obesidad"
+                end
+
+        end
+
+        def porcentajeGrasa
+                imc = (peso/(talla*talla)).round(2)
+                porcentajeGrasa = (1.2 * imc + 0.23 * @edad - 10.8 * @sexo - 5.4).round(2)
+
+                return "(#{porcentajeGrasa})"
+        end
+
+        def RCC
+
+                rcc = circunferenciaCintura.to_f/circunferenciaCadera.to_f
+
+                if sexo == 1 #hombres
+
+                        if 0.83<=rcc && rcc<0.88
+                        "(#{rcc})" #"RCC = #{rcc};  Riesgo: Bajo"
+
+                        elsif 0.88<=rcc && rcc<0.95
+                        "(#{rcc})" #"RCC = #{rcc};  Riesgo: Moderado"
+
+                        elsif 0.95<=rcc && rcc<1.01
+                        "(#{rcc})" #"RCC = #{rcc};  Riesgo: Alto"
+
+                        elsif rcc>=1.01
+                        "(#{rcc})" #"RCC = #{rcc};  Riesgo: Muy alto"
+                        end
+                elsif sexo == 0 #mujeres
+
+                        if 0.72<=rcc && rcc<0.75
+                        "(#{rcc})"#"RCC = #{rcc};  Riesgo: Bajo"
+
+                        elsif 0.78<=rcc && rcc<0.82
+                        "(#{rcc})"#"RCC = #{rcc};  Riesgo: Moderado"
+
+                        elsif rcc>=0.82
+                        "(#{rcc})"#"RCC = #{rcc};  Riesgo: Alto"
+                        end
+                end
+        end
+
+        def pliegue (pliegues)
+
+                media = 0
+
+                pliegues.length.times do |i|
+                        media +=  pliegues[i].to_f
+                end
+
+                media = (media/3).round(2)
+
+                "(#{media})"
+        end
+
+end
+
+
+
--- spec/persona_spec.rb
+++ spec/persona_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe Persona do
 	
 	before :each do
 		
-		@pe1 = Persona.new("Sonia","Díaz Santos", 22, "Candelaria", "666666666");
+		@pe1 = Persona.new("Sonia","Díaz Santos", 22,0, "Candelaria", "666666666");
 
 	end
 	
@@ -11,11 +11,18 @@ RSpec.describe Persona do
 		expect(@pe1.nombre).to eq("Sonia")
 		expect(@pe1.apellidos).to eq("Díaz Santos")
 		expect(@pe1.edad).to eq(22)
+		expect(@pe1.sexo).to eq(0)
                 expect(@pe1.direccion).to eq("Candelaria")
                 expect(@pe1.telefono).to eq("666666666")
-	
+		
 
 	end
+
+	it "Comprobando la etiqueta formateada" do
+
+		expect(@pe1.to_s).to eq("Nombre: Sonia, Apellidos: Díaz Santos, Edad: 22, Direccion: Candelaria, Telefono: 666666666")
+	
+	end 
 
 
 
