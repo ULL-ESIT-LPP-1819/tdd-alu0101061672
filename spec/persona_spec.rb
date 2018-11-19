afe8c1828c1d5f20f50cc453b98623f5fd2adfbe
Sonia Díaz Santos <alu0101061672@ull.edu.es>
2018-11-19 17:04:00 +0000

    Fallo por ausencia de initialize

--- spec/persona_spec.rb
+++ spec/persona_spec.rb
@@ -1,6 +1,17 @@
 RSpec.describe Persona do
+	
+	before :each do
+		
+		@pe1 = Persona.new("Sonia","Díaz Santos", 22, "Candelaria", "666666666");
 
+	end
+	
+	it "Comprobando el almacenamiento de datos" do
+		
+		expect(@pe1.nombre).to eq("Sonia")
+	
 
+	end
 
 
 
