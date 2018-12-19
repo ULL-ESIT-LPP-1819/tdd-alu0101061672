6e7cef36f855ce47949260ef99cba101e9d8075b
Sonia Díaz Santos <alu0101061672@ull.edu.es>
2018-12-19 16:38:47 +0000

    Hecho el código de ordenamiento each para la clase Array

--- lib/prct06/array.rb
+++ lib/prct06/array.rb
@@ -2,25 +2,38 @@ class Array
 
 	def buclefor
 
-          swapped = true
+          aux = true
             for i in 0 ...(self.length-1)
-            swapped = false
+            aux = false
               for j in 0 ... (self.length-i-1)
                 if self[j] > self[j+1]
-                  self[j] , self[j+1] = self[j+1], self[j]
-                  swapped = true
+                  
+		  self[j] , self[j+1] = self[j+1], self[j]
+                  aux = true
+
                 end
               end
-            break if swapped == false
+            break if aux == false
             end		
 	    self
 
 	end
 
-#	def each
+	def caso_each
 
+		aux = true
+         	self.each_index do |i|
+         	aux = false
+         	self.each_index do |j|
+            		if self[i] < self[j]
+	             		self[i], self[j] = self[j], self[i]
+                		aux = true
+              		end
+            	end
+          	break if aux == false
+          	end
+        	self		
 
-
-#	end
+	end
 
 end
